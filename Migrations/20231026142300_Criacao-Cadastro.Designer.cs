c27d7dbe648b5d96ff0571fe1b0c3758fd4ed572
FelipeBariri <ephzip@gmail.com>
2023-10-26 11:25:11 -0300

    Cadastro

--- Migrations/20231026142300_Criacao-Cadastro.Designer.cs
+++ Migrations/20231026142300_Criacao-Cadastro.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjetoGame.Models;
 
@@ -10,9 +11,11 @@ using ProjetoGame.Models;
 namespace ProjetoGame.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20231026142300_Criacao-Cadastro")]
+    partial class CriacaoCadastro
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
