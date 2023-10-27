027ec18d29dc20aea60bfdfc07e9472879e51f16
vspieri <viniciuspieri1632@gmail.com>
2023-10-27 11:33:05 -0300

    colocado branco

--- Program.cs
+++ Program.cs
@@ -12,11 +12,11 @@ builder.Services.AddControllersWithViews();
 /*builder.Services.AddDbContext<Contexto> //Felipe
     (options => options.UseSqlServer("Data Source=SB-1490632\\SQLSENAI;Initial Catalog = ProjetoGame;Integrated Security = True;TrustServerCertificate = True"));*/
 
-builder.Services.AddDbContext<Contexto> //Gabriel
-    (options => options.UseSqlServer("Data Source=SB-1490657\\SQLSENAI;Initial Catalog = ProjetoGame;Integrated Security = True;TrustServerCertificate = True"));
+/*builder.Services.AddDbContext<Contexto> //Gabriel
+    (options => options.UseSqlServer("Data Source=SB-1490657\\SQLSENAI;Initial Catalog = ProjetoGame;Integrated Security = True;TrustServerCertificate = True"));*/
 
-/*builder.Services.AddDbContext<Contexto> //Vinicius
-    (options => options.UseSqlServer("Data Source=SB-1490631\\SQLSENAI;Initial Catalog = ProjetoGame;Integrated Security = True;TrustServerCertificate = True"));*/
+builder.Services.AddDbContext<Contexto> //Vinicius
+    (options => options.UseSqlServer("Data Source=SB-1490631\\SQLSENAI;Initial Catalog = ProjetoGame;Integrated Security = True;TrustServerCertificate = True"));
 
 var app = builder.Build();
 
