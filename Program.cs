5ab4a97464f4034d94e8cc101e473a6535dc22f7
CelesBariri <rafael.celes.gf@gmail.com>
2023-10-27 10:01:04 -0300

    Icone de login

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-/*builder.Services.AddDbContext<Contexto> //Rafael
-    (options => options.UseSqlServer("Data Source=SB-1490645\\SQLSENAI;Initial Catalog = ProjetoGame;Integrated Security = True;TrustServerCertificate = True"));*
+builder.Services.AddDbContext<Contexto> //Rafael
+    (options => options.UseSqlServer("Data Source=SB-1490645\\SQLSENAI;Initial Catalog = ProjetoGame;Integrated Security = True;TrustServerCertificate = True"));
 
 /*builder.Services.AddDbContext<Contexto> //Felipe
     (options => options.UseSqlServer("Data Source=SB-1490632\\SQLSENAI;Initial Catalog = ProjetoGame;Integrated Security = True;TrustServerCertificate = True"));*/
@@ -15,8 +15,8 @@ builder.Services.AddControllersWithViews();
 /*builder.Services.AddDbContext<Contexto> //Gabriel
     (options => options.UseSqlServer("Data Source=SB-1490657\\SQLSENAI;Initial Catalog = ProjetoGame;Integrated Security = True;TrustServerCertificate = True"));*/
 
-builder.Services.AddDbContext<Contexto> //Vinicius
-    (options => options.UseSqlServer("Data Source=SB-1490631\\SQLSENAI;Initial Catalog = ProjetoGame;Integrated Security = True;TrustServerCertificate = True"));
+/*builder.Services.AddDbContext<Contexto> //Vinicius
+    (options => options.UseSqlServer("Data Source=SB-1490631\\SQLSENAI;Initial Catalog = ProjetoGame;Integrated Security = True;TrustServerCertificate = True"));*/
 
 var app = builder.Build();
 
