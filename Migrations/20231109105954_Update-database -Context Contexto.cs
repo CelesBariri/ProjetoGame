cdc4991da42bfbc8d0adfe50d756e06caaf9e2ec
FelipeBariri <ephzip@gmail.com>
2023-11-09 09:44:30 -0300

    wfe

---
+++ Migrations/20231109105954_Update-database -Context Contexto.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetoGame.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedatabaseContextContexto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
